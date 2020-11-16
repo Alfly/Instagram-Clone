9c185f4ac413d72e7298d840482145750848b322
Alfly <alfly@Alflys-Mac.local>
2020-11-15 17:50:19 -0800

    Test

---
+++ Instagram/Models/Models.swift
@@ -0,0 +1,9 @@
+//
+//  Models.swift
+//  Instagram
+//
+//  Created by Alfly on 2020/11/14.
+//  Copyright © 2020 Alfly. All rights reserved.
+//
+
+import Foundation
---
+++ Instagram/Views/NoNotificationsView.swift
@@ -0,0 +1,21 @@
+//
+//  noNotificationsView.swift
+//  Instagram
+//
+//  Created by Alfly on 2020/11/15.
+//  Copyright © 2020 Alfly. All rights reserved.
+//
+
+import UIKit
+
+class noNotificationsView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
